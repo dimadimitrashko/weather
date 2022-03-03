ba9dfa5312563182beb068d455e696cb1da209b7
Dimas <dimirtashko@gmail.com>
2022-03-03 09:50:49 +0100

    changes

--- lib/api/weather_api.dart
+++ lib/api/weather_api.dart
@@ -32,6 +32,7 @@ class WeatherApi {
       parameters = params;
     }
 
+
     var uri = Uri.https(Constants.WEATHER_BASE_URL_DOMAIN, Constants.WEATHER_FORECAST_PATH, parameters);
     log('request: ${uri.toString()}');
 
